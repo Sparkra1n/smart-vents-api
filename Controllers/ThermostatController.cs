b93536395f84bd27c943b9c2e9d9369017ce9913
Ani <sparkra1n@proton.me>
2023-10-09 15:35:42 -0700

    fixed ventHistories, addVent

--- Controllers/ThermostatController.cs
+++ Controllers/ThermostatController.cs
@@ -2,20 +2,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using smart_vents_api.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace smart_vents_api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ThermostatController : ControllerBase 
+public class ThermostatController : ControllerBase
 {
     private static DashboardSettings Settings = new();
     private static readonly List<Vent> Vents = new();
     private readonly ILogger<ThermostatController> Logger;
-    public ThermostatController(ILogger<ThermostatController> logger) 
+    public ThermostatController(ILogger<ThermostatController> logger)
     {
         Logger = logger;
-        Settings = new DashboardSettings() 
+        Settings = new DashboardSettings()
         {
             IsEnabled = false,
             MasterTargetTemp = 25
@@ -24,11 +25,11 @@ public class ThermostatController : ControllerBase
 
     [HttpPost]
     [Route("setVentTargetTemp")]
-    public ActionResult SetVentTargetTemp(string id, double targetTemp) 
+    public ActionResult SetVentTargetTemp(string id, double targetTemp)
     {
-        foreach (var vent in Vents) 
+        foreach (var vent in Vents)
         {
-            if (vent.Id == id) 
+            if (vent.Id == id)
             {
                 vent.TargetTemp = targetTemp;
                 return Ok();
@@ -40,54 +41,61 @@ public class ThermostatController : ControllerBase
     // Vent posts its info and is told whether it should be open or closed
     [HttpPost]
     [Route("updateVent")]
-public ActionResult<bool> UpdateVent(VentSummary payload)
-{
-    var ventToUpdate = Vents.FirstOrDefault(vent => vent.Id == payload.Id);
-
-    if (ventToUpdate == null)
-        return NotFound();
-    
-    ventToUpdate.History.Add(new VentData()
+    public ActionResult<bool> UpdateVent(VentSummary payload)
     {
-        Timestamp = DateTime.UtcNow.ToString("u"),
-        Temp = payload.Temp ?? 0,
-        IsOccupied = payload.IsOccupied ?? false
-    });
+        var ventToUpdate = Vents.FirstOrDefault(vent => vent.Id == payload.Id);
 
-    if (!Settings.IsEnabled)
-        return true;
+        if (ventToUpdate == null)
+            return NotFound();
 
-    if (!payload.IsOccupied ?? false)
-        return false;
+        ventToUpdate.History.Add(new VentData()
+        {
+            Timestamp = DateTime.UtcNow.ToString("u"),
+            Temp = payload.Temp ?? 0,
+            IsOccupied = payload.IsOccupied ?? false
+        });
 
-    return payload.Temp <= ventToUpdate.TargetTemp;
-}
+        if (!Settings.IsEnabled)
+            return true;
+
+        if (!payload.IsOccupied ?? false)
+            return false;
+
+        return payload.Temp <= ventToUpdate.TargetTemp;
+    }
 
 
     [HttpPost]
     [Route("addVent")]
-    public void AddVent(string id) 
+    public ActionResult AddVent(string id)
     {
-        Vents.Add(new Vent() 
+        if (Vents.Any(vent => vent.Id == id))
+        {
+            return BadRequest();
+        }
+
+        Vents.Add(new Vent()
         {
             Id = id,
             TargetTemp = 25,
-            History = new List<VentData>()   
+            History = new List<VentData>()
         });
+
+        return Ok();
     }
 
     [HttpPost]
     [Route("settings")]
-    public void SetDashboardSettings(DashboardSettings dashboardData) 
+    public void SetDashboardSettings(DashboardSettings dashboardData)
     {
         Settings = dashboardData;
     }
 
     [HttpGet]
     [Route("ventHistory")]
-    public ActionResult<List<VentData>> GetVentHistory(string id) 
+    public ActionResult<List<VentData>> GetVentHistory(string id)
     {
-        foreach (var vent in Vents) 
+        foreach (var vent in Vents)
         {
             if (vent.Id == id)
                 return vent.History;
@@ -97,22 +105,28 @@ public ActionResult<bool> UpdateVent(VentSummary payload)
 
     [HttpGet]
     [Route("ventHistories")]
-    public string GetVentHistories() 
+    public string GetVentHistories()
     {
-        return JsonConvert.SerializeObject(Vents, Formatting.Indented);
+        var settings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        return JsonConvert.SerializeObject(Vents, settings);
     }
 
     [HttpGet]
     [Route("ventSummaries")]
-    public List<VentSummary> GetVentSummaries() 
+    public List<VentSummary> GetVentSummaries()
     {
         List<VentSummary> ventSummaries = new();
 
-        foreach (var vent in Vents) 
+        foreach (var vent in Vents)
         {
             var lastHistory = vent.History.LastOrDefault();
 
-            ventSummaries.Add(new VentSummary() 
+            ventSummaries.Add(new VentSummary()
             {
                 Id = vent.Id,
                 TargetTemp = vent.TargetTemp,
